b74c61e0d71ea1c06f70741c8deca1bf16d46a11
Gennady Denisov <denisovgena@gmail.com>
2014-01-02 17:14:46 +0400

    Add tag cloud page. Add main navigation.

--- bookmarks/apps/tags/tests/test_views.py
+++ bookmarks/apps/tags/tests/test_views.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 import json
+import os
 from urlparse import urlparse
 from django.core.urlresolvers import reverse
 
 from django.test import TestCase
-
+from django.test.utils import override_settings
 
 from ..models import Tag
 from ...bookmarks.models import Bookmark, Link
@@ -14,19 +15,16 @@ from ...users.models import BookmarksUser
 __author__ = 'Gennady Denisov <denisovgena@gmail.com>'
 
 
+@override_settings(SECRET_KEY='FooBar')
 class TagsViewTestCase(TestCase):
+    fixtures = [os.path.join(os.path.dirname(__file__),
+                             '../../bookmarks/tests/users.json'),
+                os.path.join(os.path.dirname(__file__), 'tag_cloud_data.json')]
+
     def setUp(self):
-        self.user = BookmarksUser.objects.create_user(
-            email='foo@bar.bz',
-            first_name='foo',
-            last_name='bar',
-            password='buz'
-        )
-        self.tag_foo = Tag.objects.create(name='foo')
-        Tag.objects.create(name='bar')
-        Tag.objects.create(name='buz')
         self.tag_autocomplete_url = reverse('tags:autocomplete')
         self.tag_url = lambda t: reverse('tags:page', kwargs={'tag_name': t})
+        self.tag_cloud_url = reverse('tags:cloud')
 
     def test_autocomplete_url(self):
         self.assertEquals(self.tag_autocomplete_url, '/tags/autocomplete/')
@@ -35,28 +33,46 @@ class TagsViewTestCase(TestCase):
         response = call_response('foo')
         self.assertEquals(response.status_code, 302)
         self.assertEquals(urlparse(response['Location']).path, '/users/login')
-        self.client.login(username=self.user.email,
+        self.client.login(username='foo@bar.bz',
                           password='buz')
         response = call_response('foo')
         self.assertEquals(response.status_code, 200)
         data = json.loads(response.content)
         self.assertEquals(len(data), 1)
 
-    def test_tag_page_view(self):
-        self.client.login(username=self.user.email,
-                          password='buz')
-        bookmark = Bookmark.objects.create(
-            link=Link.objects.create(url='http://www.foo.bar/'),
-            title='Foo', user=self.user)
-        bookmark.tag_set.add(self.tag_foo)
-        bookmark.save()
-        tag_name = 'foo'
+    def tag_name_bookmarks(self, tag_name, bookmarks):
         self.assertEquals(self.tag_url(tag_name), '/tags/{0}/'.format(tag_name))
         response = self.client.get(self.tag_url(tag_name))
         self.assertEquals(response.status_code, 200)
-        self.assertEquals(response.context['tag_name'], 'foo')
+        self.assertEquals(response.context['tag_name'], tag_name)
         self.assertTemplateUsed(response, 'tags/tag_list.html')
-        self.assertEquals(len(response.context['bookmark_list']), 1)
+        self.assertEquals(len(response.context['bookmark_list']), bookmarks)
 
+    def test_tag_page_view(self):
+        self.client.login(username='foo@bar.bz',
+                          password='buz')
+        # In fixtures we have 10 bookmarks for tag foo
+        self.tag_name_bookmarks('foo', 10)
+        # In fixtures we have 5 bookmarks for tag bar
+        self.tag_name_bookmarks('bar', 5)
+        # In fixtures we have 2 bookmarks for tag buz
+        self.tag_name_bookmarks('buz', 2)
 
-
+    def test_tag_cloud_page(self):
+        self.assertEquals(self.tag_cloud_url, '/tags/',
+                          msg='URL should be /tags/')
+        self.client.login(username='foo@bar.bz',
+                          password='buz')
+        response = self.client.get(self.tag_cloud_url)
+        self.assertEquals(200, response.status_code)
+        self.assertTemplateUsed(response, 'tags/tag_cloud.html')
+        tags = response.context['tag_list']
+        self.assertEquals(len(tags), 3)
+        # Test weights
+        for tag in tags:
+            if tag.name == 'foo':
+                self.assertEquals(tag.weight, 22)
+            elif tag.name == 'bar':
+                self.assertEquals(tag.weight, 8)
+            elif tag.name == 'buz':
+                self.assertEquals(tag.weight, 8)
